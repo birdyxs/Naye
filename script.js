994c9cbeecb108816eac9fadfe6108c78f098316
mar <105406641+birdyxs@users.noreply.github.com>
2026-02-01 11:52:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -23,3 +23,22 @@ const playlist = [
 
     window.onload = () => loadSong(currentTrackIndex);
     audio.onended = nextSong;
+
+
+function createHeart() {
+        const heart = document.createElement('div');
+        heart.classList.add('heart-fall');
+        heart.innerHTML = '❤'; 
+        heart.style.left = Math.random() * 100 + "vw";
+        heart.style.animationDuration = Math.random() * 3 + 2 + "s";
+        heart.style.opacity = Math.random();
+        heart.style.fontSize = Math.random() * 10 + 10 + "px";
+
+        document.body.appendChild(heart);
+
+        setTimeout(() => {
+            heart.remove();
+        }, 5000);
+    }
+
+    setInterval(createHeart, 500); 
